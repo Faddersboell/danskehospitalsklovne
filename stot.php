5af06d17bffc76e774fcad02d60da67eab178c10
Faddersboell <Faddersboell@kamillaf91gmailcom.local>
2013-12-12 12:45:36 +0100

    stot

--- stot.php
+++ stot.php
@@ -1,5 +1,12 @@
 <?php include 'includes/header.inc.php';?>
 
-
-
+<article class="stot">
+	<h1>Støt os</h1>
+	  <h4>Her har du mulighed for at vælge hvordan du vil støtte os</h4>
+	  
+      <div><img src="images/mobil.png" alt="stotmobil"></div>
+      <div><img src="images/kreditkort.jpg" alt="stotkort"></div>
+      
+</article>
+      
 <?php include 'includes/footer.inc.php';?>
