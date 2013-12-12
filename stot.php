e7950036b5dc01402bd0b3930eb64134dfc6c565
Faddersboell <Faddersboell@kamillaf91gmailcom.local>
2013-12-12 14:46:35 +0100

    støt os

--- stot.php
+++ stot.php
@@ -4,8 +4,8 @@
 	<h1>Støt os</h1>
 	  <h4>Her har du mulighed for at vælge hvordan du vil støtte os</h4>
 	  
-      <div><img src="images/mobil.png" alt="stotmobil"></div>
-      <div><img src="images/kreditkort.jpg" alt="stotkort"></div>
+      <div id="mobil"><img src="images/mobil.png" alt="stotmobil"></div>
+      <div id="kort"><img src="images/kreditkort.jpg" alt="kort"></div>
       
       
 </article>
