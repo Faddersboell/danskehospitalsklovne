b0cad44e15ce4135a2c7566931276b69bfeaa69c
Faddersboell <Faddersboell@kamillaf91gmailcom.local>
2013-12-13 10:37:58 +0100

    lagt op

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no">
 <title>Danske hospitalsklovnes web app</title>
 <link rel="stylesheet" type="text/css" href="css/reset.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
