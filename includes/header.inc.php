a24dc762a30d6047afba087794d640b3e7483790
Faddersboell <Faddersboell@kamillaf91gmailcom.local>
2013-12-13 12:17:29 +0100

    skrifttype

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -6,6 +6,8 @@
 <title>Danske hospitalsklovnes web app</title>
 <link rel="stylesheet" type="text/css" href="css/reset.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
+
+
 </head>
 
 <body <?php echo $id;?>>
