3a9b5306f44089f0a190b433ed4951d368604939
Faddersboell <Faddersboell@kamillaf91gmailcom.local>
2013-12-16 14:31:14 +0100

    sendmail

--- kontakt.php
+++ kontakt.php
@@ -4,12 +4,14 @@ include 'includes/header.inc.php';?>
 
 <h1>Her kan du kontakte os</h1>
 
-    <form  action="" method="POST" enctype="multipart/form-data">
-        <input type="text" name="navn" id="navn" placeholder="Navn"/> <br />
-        <input name="email" type="text" id="email" placeholder="Email"/> <br />
-        <textarea name="besked" rows="15" cols="30"> </textarea> <br />
-        <input type="submit" class="button" value="Send email"/>
-    </form> 
+    <form action="sendmail.php" method="post">
+    	
+        <input type="text" name="navn" id="navn" placeholder="Navn"/> <br/>
+        <br/>
+        <input type="text" name="email" id="email" placeholder="Email"/> <br/>
+        <br/>
+        <textarea name="besked"></textarea><br/>
+        <input type="submit" name="submit" value="Send mail"/>
 
 	
 <?php include 'includes/footer.inc.php';?>
---
+++ sendmail.php
@@ -0,0 +1,7 @@
+<?php
+$id = "";
+include 'includes/header.inc.php';?>
+
+
+	
+<?php include 'includes/footer.inc.php';?>
