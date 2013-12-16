21cb01ec0e40d7c8c747557be11c272bab046e96
Faddersboell <Faddersboell@kamillaf91gmailcom.local>
2013-12-16 14:13:33 +0100

    kontakt

--- kontakt.php
+++ kontakt.php
@@ -2,7 +2,14 @@
 $id = "";
 include 'includes/header.inc.php';?>
 
+<h1>Her kan du kontakte os</h1>
 
+    <form  action="" method="POST" enctype="multipart/form-data">
+        <input type="text" name="navn" id="navn" placeholder="Navn"/> <br />
+        <input name="email" type="text" id="email" placeholder="Email"/> <br />
+        <textarea name="besked" rows="15" cols="30"> </textarea> <br />
+        <input type="submit" class="button" value="Send email"/>
+    </form> 
 
 	
 <?php include 'includes/footer.inc.php';?>
